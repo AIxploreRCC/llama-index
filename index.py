f31ff989e9d5c0b5a74b0e459eb3e4336d5c9809
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2023-04-30 09:54:13 +0200

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import os, streamlit as st
 
 import openai 
 
-openai.api_key = st.secrets["pass"]
+openai.api_key = st.secrets['pass']
 
 # Create Text Area Widget to enable user to enter texts
 article_text = st.text_area('Enter your scientific texts to summarize')
