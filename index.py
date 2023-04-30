0dc68c154f328e9f9add56ea6b90b8c7333cd243
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2023-04-30 09:54:42 +0200

    Update index.py

--- index.py
+++ index.py
@@ -8,3 +8,7 @@ openai.api_key = st.secrets['pass']
 # Create Text Area Widget to enable user to enter texts
 article_text = st.text_area('Enter your scientific texts to summarize')
 
+# Create Radio Buttons
+output_size = st.radio( label = “What kind of output do you want?”, 
+                        options= [“To-The-Point”, “Concise”, “Detailed”]
+                     )
